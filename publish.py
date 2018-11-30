6715f171d7b81d3c09d3fd337594cdf9e0a1a386
root <jumajumo@jbuck.de>
2018-11-30 08:26:37 +0000

    add will, qos and retain flag to publish messages

--- publish.py
+++ publish.py
@@ -14,21 +14,24 @@ thingTopic = "jumajumo/" + thingid + "/"
 refreshTopic = thingTopic + "env/refresh"
 
 client = mqtt.Client(thingid)
+
+client.will_set(thingTopic, "undef", qos=1, retain=True)
+
 client.connect(brokeraddr)
 
-client.publish(thingTopic, str(datetime.datetime.now()))
-client.publish(thingTopic + "sys/type", "sensor")
-client.publish(thingTopic + "sys/device", "dht22")
+client.publish(thingTopic, str(datetime.datetime.now()), qos=1, retain=True)
+client.publish(thingTopic + "sys/type", "sensor", qos=1, retain=True)
+client.publish(thingTopic + "sys/device", "dht22", qos=1, retain=True)
 
-client.publish(refreshTopic, refresh)
+client.publish(refreshTopic, refresh, qos=1, retain=True)
 
 try:
     while True:
 
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, pin)
 
-        client.publish(thingTopic + "humidity",humidity)
-        client.publish(thingTopic + "temperature", temperature)
+        client.publish(thingTopic + "humidity",humidity, qos=0, retain=True)
+        client.publish(thingTopic + "temperature", temperature, qos=0, retain=True)
 
         time.sleep(refresh)
 
